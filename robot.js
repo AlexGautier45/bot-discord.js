b208b3dd82273318029133bba254fdb5f7f69006
lulu5239 <32999164+lulu5239@users.noreply.github.com>
2018-07-19 12:29:20 +0200

    Ajout de commandes.

--- robot.js
+++ robot.js
@@ -4,14 +4,19 @@ const client = new Discord.Client();
 
 // Variables :
 var prefixe = "::"
+var admins = ["426775253650505729", "Votre ID."]
 
 // Au lancement :
 client.on("ready", () => {
   console.log("Prêt !") // Prêt !
-}
+})
 
 // A un message :
 client.on("message", msg => {
   if(msg.content.startsWith(prefixe+"eval ") && admins.includes(msg.author.id)) {
     eval(msg.content.replace(prefixe+"eval"))
   }
+  if(msg.content == prefixe+"ping") {
+    msg.channel.send("Pong !")
+  }
+})
